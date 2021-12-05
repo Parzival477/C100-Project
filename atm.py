e8a65cdd1f6fdc8b4e14b959c99278f15b4e16e5
Parzival477 <62180607+Parzival477@users.noreply.github.com>
2021-12-05 14:50:26 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,19 @@
+class User(object):
+    def __init__(self, name, age, gender, level, pin=None):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.level = level
+        self.pin = pin or {}
+
+    def getPin():
+        pin = input("Please enter your pin")
+
+    def CashWithdrawal():
+        MoneyToBeWithdrawed = input("How much money would you like to withdraw?")
+        print(MoneyToBeWithdrawed + "Has beem transfered to your account")    s
+
+    def Balance():
+        print(MoneyToBeWithdrawed+ "Is the curremt balance of your account. If there is an error, check with your bank")    
+
+   
